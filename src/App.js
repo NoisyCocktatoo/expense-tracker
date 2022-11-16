595278a9ff0c3be98e368ca5edccfa47e4871d38
Dave <jdave.grencio@gmail.com>
2022-11-16 13:28:56 +0800

    1.3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import AddTransact from './components/AddTransact';
-import Balance from './components/Balance';
-import Income from './components/Income';
-import Title from './components/Title'
-import Transactions from './components/Transactions';
+import "./App.css";
+import AddTransact from "./components/AddTransact";
+import Balance from "./components/Balance";
+import Income from "./components/Income";
+import Title from "./components/Title";
+import Transactions from "./components/Transactions";
 
-import { GlobalProvider } from './context/GlobalState';
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
     <GlobalProvider>
       <Title />
-      <div className='container'>
+      <div className="container">
         <Balance />
         <Income />
         <Transactions />
